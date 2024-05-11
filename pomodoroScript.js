ba155757c96f534b600d917a83a5481bd50cbafe
Levi <92650678+LeviDomingos@users.noreply.github.com>
2024-05-11 06:57:35 +0100

    Update pomodoroScript.js
    used just class

--- pomodoroScript.js
+++ pomodoroScript.js
@@ -1,155 +1,258 @@
-let startandStopTime = true;
-let breakSessionStart = true;
-let keepTrackOfTime = 0;
-let breakDecrementAndIncrement = 5;
-let sessionDecremenetAndIncrement = 25;
-let getMinutes = 25; 
-let getSeconds = "00";
-let sessionTitle ="Session";
+class TakeAwayAndAdd {
+    constructor() {
+    }
+
+    showResul(val, sign) {
+        if(sign === "-") {
+            return val -= parseInt(1);
+        }
+        if(sign === "+") {
+            return val += parseInt(1);
+        }
+    }
+}
+
+class ClassTrueOrFalse {
+    constructor() { 
+        this.startandStopTime = true;
+        this.keepTrackOfTime = 0;
+        this.sessionTitle ="Session";
+    }
+
+    showStartAndStop = () => this.startandStopTime;
+
+    resetClassTrueFalse() {
+        this.startandStopTime = true;
+        this.keepTrackOfTime = 0;
+        this.sessionTitle = "Session";
+    }
+}
+
+class ClassSession extends TakeAwayAndAdd {
+    constructor() {
+        super()
+        this.sessionDecremenetAndIncrement = 25;
+    }
+
+    sessionDecrement() {
+        if(this.sessionDecremenetAndIncrement > 10) {
+            const result = this.sessionDecremenetAndIncrement = this.showResul(this.sessionDecremenetAndIncrement, "-");
+            return result;
+        } else {
+            if(this.sessionDecremenetAndIncrement > 1 ) {
+                this.sessionDecremenetAndIncrement = this.showResul(this.sessionDecremenetAndIncrement, "-");
+                const result = "0" + this.sessionDecremenetAndIncrement;
+                return result;
+            } else {
+                const result = "0" + this.sessionDecremenetAndIncrement;
+                return result;
+            }
+        }
+    }
+
+    sessionIncrement() {
+        if(this.sessionDecremenetAndIncrement < 10) {
+            this.sessionDecremenetAndIncrement = this.showResul(this.sessionDecremenetAndIncrement, "+");
+            if(this.sessionDecremenetAndIncrement === 10) {
+                const result =  this.sessionDecremenetAndIncrement;
+                return result;
+            } else {
+                const result = "0" + this.sessionDecremenetAndIncrement;
+                return result;
+            }
+        } else {
+            if(this.sessionDecremenetAndIncrement < 60 ) {
+                const result = this.sessionDecremenetAndIncrement = this.showResul(this.sessionDecremenetAndIncrement, "+");
+                return result;
+            } else {
+                const result = this.sessionDecremenetAndIncrement;
+                return result;
+            }
+        } 
+        
+    }
+
+    showSessionLength() {
+        return this.sessionDecremenetAndIncrement;
+    }
+
+    resetSessionDecremenetAndIncrement = () => this.sessionDecremenetAndIncrement = 25;
+}
+class ClassMinutesAndSecond extends TakeAwayAndAdd {
+    constructor() {
+        super()
+        this.minutes = 25; 
+        this.seconds = "00";
+        this.count = 60;
+    }
+
+    hourFormat(min, sec) {
+        return min + ":" + sec;
+    }
+
+    countDownMinutes() {
+        this.minutes = this.showResul(this.minutes, "-");
+        if(this.minutes < 10) {
+            return "0" + this.minutes;
+        } else {
+            return this.minutes;
+        }
+    }
+
+    countDownSeconds() {
+        this.seconds = this.showResul(this.count, "-");
+        this.count = this.seconds;
+        if(this.count < 10) {
+            return "0" + this.seconds;
+        } else {
+            return this.seconds;
+        }
+    }
+
+    resetClassMinutesAndSeconds() {
+        this.minutes = 25;
+        this.seconds =  "00";
+        this.count = 60;
+    }
+}
+
+class ClassBreak extends TakeAwayAndAdd{
+    constructor() {
+        super()
+        this.breakDecrementAndIncrement = 5;
+    }
+
+    breakDecrement() {
+        if(this.breakDecrementAndIncrement > 1) {
+            return this.breakDecrementAndIncrement = this.showResul(this.breakDecrementAndIncrement, "-");
+        }
+    }
+
+    breakIncrement() {
+        if(this.breakDecrementAndIncrement < 60 ) {
+            return this.breakDecrementAndIncrement = this.showResul(this.breakDecrementAndIncrement, "+");
+        }
+    }
+
+    showBreakLength() {
+        return this.breakDecrementAndIncrement;
+    } 
+
+    resetBreakDecrementAndIncrement = () => this.breakDecrementAndIncrement = 5;
+}
 
 $(document).ready(function () {
-    $("#break-length").text(breakDecrementAndIncrement);
-    $("#session-length").text(sessionDecremenetAndIncrement);
-    $("#timer-label").text(sessionTitle);
+
+    const startBreak = new ClassBreak();
+    const startSession = new ClassSession();
+    const setMinuteAndSecond = new ClassMinutesAndSecond();
+    const trueOrFalse = new ClassTrueOrFalse();
+
+    $("#break-length").text(startBreak.showBreakLength());
+    $("#session-length").text(startSession.showSessionLength());
+    $("#timer-label").text(trueOrFalse.sessionTitle);
     $("#break-decrement").on("click", breakDecrement);
     $("#break-increment").on("click", breakIncrement);
     $("#session-decrement").on("click", sessionDecrement);
     $("#session-increment").on("click", sessionIncrement);
     $("#start_stop").on("click", startStopTime);
     $("#reset").on("click", resetTheClock);
-    $("#time-left").text(hourFormat(getMinutes, getSeconds));
-
+    $("#time-left").text(setMinuteAndSecond.hourFormat(setMinuteAndSecond.minutes, setMinuteAndSecond.seconds));
+   
 
     function breakDecrement() {
-        if(startandStopTime) {
-            if(breakDecrementAndIncrement > 1) {
-                $("#break-length").text(breakDecrementAndIncrement -= 1);
-            }
+        if(trueOrFalse.showStartAndStop()) {
+            const cls = startBreak.breakDecrement();
+            $("#break-length").text(cls);
         }
     }
 
     function breakIncrement() {
-        if(startandStopTime) {
-            if(breakDecrementAndIncrement < 60 ) {
-                $("#break-length").text(breakDecrementAndIncrement += 1);
-            }
+        if(trueOrFalse.showStartAndStop()) {
+            const cls = startBreak.breakIncrement();
+            $("#break-length").text(cls);
         }
-       
     }
+
     function sessionDecrement() {
-        if(startandStopTime) {
-            getSeconds = "00";
-            if(sessionDecremenetAndIncrement > 1) {
-                sessionDecremenetAndIncrement = sessionDecremenetAndIncrement - 1;
-                getMinutes =  sessionDecremenetAndIncrement;
-                $("#session-length").text(sessionDecremenetAndIncrement);
-                if(getMinutes > 9) {
-                    $("#time-left").text(hourFormat(getMinutes, getSeconds));
-                }
-                else {
-                    getMinutes = "0" + getMinutes;
-                    $("#time-left").text(hourFormat(getMinutes, getSeconds));
-                }
-               
-            } 
-        }      
+        if(trueOrFalse.showStartAndStop()) { 
+            const cls = startSession.sessionDecrement();
+            setMinuteAndSecond.minutes = cls;
+            setMinuteAndSecond.seconds = "00";
+            setMinuteAndSecond.count = 60;
+            $("#session-length").text(cls);
+            $("#time-left").text(setMinuteAndSecond.hourFormat(cls, setMinuteAndSecond.seconds));     
+        }
     }
 
     function sessionIncrement() {
-        if(startandStopTime) {
-            getSeconds = "00";
-            if(sessionDecremenetAndIncrement < 60) {
-                sessionDecremenetAndIncrement = sessionDecremenetAndIncrement + 1;
-                getMinutes = sessionDecremenetAndIncrement;
-                $("#session-length").text(sessionDecremenetAndIncrement );
-
-                if(getMinutes > 9) {
-                    $("#time-left").text(hourFormat(getMinutes, getSeconds));
-                } else {
-                    getMinutes = "0" + getMinutes;
-                    $("#time-left").text(hourFormat(getMinutes, getSeconds));
-                }
-            }
+        if(trueOrFalse.showStartAndStop()) { 
+            const cls = startSession.sessionIncrement();
+            setMinuteAndSecond.minutes = cls;
+            setMinuteAndSecond.seconds = "00";
+            setMinuteAndSecond.count = 60;
+            $("#session-length").text(cls);
+            $("#time-left").text(setMinuteAndSecond.hourFormat(cls, setMinuteAndSecond.seconds));   
         }
-    }
-
-    function hourFormat(minutes, seconds) {
-        return minutes + ":" + seconds;
     }
 
     function startStopTime() {
-        if(startandStopTime) {
-            keepTrackOfTime = setInterval(startTickingTime, 1000);
-            startandStopTime = false;
+        if(trueOrFalse.showStartAndStop()) {
+            trueOrFalse.keepTrackOfTime = setInterval(startTickingTime, 1000);
+            trueOrFalse.startandStopTime = false;
         }
         else {
-            if(!startandStopTime) {
-                clearInterval(keepTrackOfTime);
-                startandStopTime = true;
+            if(!trueOrFalse.showStartAndStop()) {
+                clearInterval(trueOrFalse.keepTrackOfTime);
+                trueOrFalse.startandStopTime = true;
             }
         }
     }
         
     function startTickingTime() {
-        if(getSeconds === "00") {
-            getMinutes = getMinutes - 1;
-            getSeconds = 59;
-            if(getMinutes > 9) {
-                $("#time-left").text(hourFormat(getMinutes, getSeconds));
-            } else {
-                getMinutes = "0" + getMinutes;
-                $("#time-left").text(hourFormat(getMinutes, getSeconds));
-            }
-           
-        } else {
-            getSeconds = getSeconds - 1;
-            if(getSeconds > 9) {
-                $("#time-left").text(hourFormat(getMinutes, getSeconds));
-            } else {
-                getSeconds = "0" + getSeconds;
-                $("#time-left").text(hourFormat(getMinutes,getSeconds));
-            }
+        setMinuteAndSecond.seconds = setMinuteAndSecond.countDownSeconds();
+       
+        if( setMinuteAndSecond.seconds === 59) {   
+            trueOrFalse.sessionTitle = "Session"; 
+            $("#timer-label").text(trueOrFalse.sessionTitle);
+            setMinuteAndSecond.minutes = setMinuteAndSecond.countDownMinutes();
+            $("#time-left").text(setMinuteAndSecond.hourFormat(setMinuteAndSecond.minutes, setMinuteAndSecond.seconds));  
+        }
+        else {
+            $("#time-left").text(setMinuteAndSecond.hourFormat(setMinuteAndSecond.minutes, setMinuteAndSecond.seconds));
 
-            if(getSeconds === "00" && getMinutes ==="00" && sessionTitle === "Break") {
-            
-                sessionTitle ="Session"; 
-                $("#timer-label").text(sessionTitle);
-                const hourformat = hourFormat(getMinutes ="00", getSeconds = "00");
-                $("#time-left").text(hourformat);
-                getMinutes = $("#session-length").text();
+            if(setMinuteAndSecond.count === 0) {
+                setMinuteAndSecond.count = 60;
             }
+        }
+        if(setMinuteAndSecond.minutes === "00" && setMinuteAndSecond.seconds === "00") {
+            trueOrFalse.sessionTitle ="Break";
+            $("#timer-label").text(trueOrFalse.sessionTitle);
+            $("#time-left").text(setMinuteAndSecond.hourFormat(setMinuteAndSecond.minutes, setMinuteAndSecond.seconds));  
+            let breakLength = startBreak.showBreakLength();
 
-            if(getSeconds === "00" && getMinutes ==="00" && sessionTitle === "Session") {
-                let s = setTimeout(startStopTime, 2000);
-                sessionTitle ='Break'; 
-                $("#timer-label").text(sessionTitle);
-                let hourformat = hourFormat(getMinutes ="00", getSeconds = "00");
-                $("#time-left").text(hourformat);
-                getMinutes = "0" + $("#break-length").text(); 
-                clearTimeout(s);
+            if(breakLength > 9) {
+                setMinuteAndSecond.minutes = "";
+                setMinuteAndSecond.minutes = breakLength;
+                $("#time-left").text(setMinuteAndSecond.hourFormat(setMinuteAndSecond.minutes, setMinuteAndSecond.seconds));  
+                setMinuteAndSecond.count = setMinuteAndSecond.resetCount(); 
+            } else {
+                setMinuteAndSecond.minutes = "";
+                setMinuteAndSecond.minutes = "0" + breakLength;
+                $("#time-left").text(setMinuteAndSecond.hourFormat(setMinuteAndSecond.minutes, setMinuteAndSecond.seconds));  
+                setMinuteAndSecond.count = setMinuteAndSecond.resetCount(); 
             }
         }
     }
 
     function resetTheClock() {
-        clearInterval(keepTrackOfTime);
-    
-        getSeconds = "00";
-        breakDecrementAndIncrement = 5;
-        sessionDecremenetAndIncrement = 25;
-        getMinutes = sessionDecremenetAndIncrement;
-        $("#time-left").text(hourFormat(getMinutes, getSeconds));
-        $("#timer-label").text("Session");
-        $("#break-length").text(breakDecrementAndIncrement);
-        $("#session-length").text(sessionDecremenetAndIncrement);
-        startandStopTime = true;
-        $('#beep').currentTime = 0;
-        
-    }
-
-    function playSound() {
-       //var audios = document.getElementById("beep").src;
-       //audios.play();
+        clearInterval(trueOrFalse.keepTrackOfTime);
+        trueOrFalse.resetClassTrueFalse();
+        setMinuteAndSecond.resetClassMinutesAndSeconds();
+        $("#time-left").text(setMinuteAndSecond.hourFormat(setMinuteAndSecond.minutes, setMinuteAndSecond.seconds));
+        $("#timer-label").text(trueOrFalse.sessionTitle);
+        $("#break-length").text(startBreak.resetBreakDecrementAndIncrement());
+        $("#session-length").text(startSession.resetSessionDecremenetAndIncrement());
         
     }
 
